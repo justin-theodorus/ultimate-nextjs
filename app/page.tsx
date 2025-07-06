cffebce4e7b5396c79d5fd93743c1135b9ef4790
justin-theodorus <justin.theodorus@gmail.com>
2025-07-06 17:31:11 +0800

    implement eslint & prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div><h1 className='text-3xl font-bold underline'>Home</h1></div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Home</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
